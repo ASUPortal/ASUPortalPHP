154cf3968c8bc8922ff54ea947597a76e12b64c0
Александр Бармин <barmin.alexander@gmail.com>
2013-03-10 13:25:37 +0600

    Добавил функцию представления даты в нужном формате

--- _core/_framework/CUtils.class.php
+++ _core/_framework/CUtils.class.php
@@ -220,4 +220,32 @@ class CUtils {
             return $many;
         }
     }
+
+    /**
+     * Представляем дату в формате дд ммммм гггг
+     *
+     * @param $date
+     * @return string
+     */
+    public static function getDateAsWord($date) {
+        $stamp = strtotime($date);
+        $res = date("d", $stamp);
+        $months = array(
+            "01" => "января",
+            "02" => "февраля",
+            "03" => "марта",
+            "04" => "апреля",
+            "05" => "мая",
+            "06" => "июня",
+            "07" => "июля",
+            "08" => "августа",
+            "09" => "сентября",
+            "10" => "октября",
+            "11" => "ноября",
+            "12" => "декабря"
+        );
+        $res .= " ".$months[date("m", $stamp)];
+        $res .= " ".date("Y", $stamp);
+        return $res;
+    }
 }
