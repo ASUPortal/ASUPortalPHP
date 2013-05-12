6431f51ed8a73a0dd376ddc3b077e6651472d0ea
Александр Бармин <barmin.alexander@gmail.com>
2013-05-12 18:09:58 +0600

    Сделал форму для добавления/удаления финансов

---
+++ _core/_controllers/CGrantMoneyController.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: aleksandr
+ * Date: 12.05.13
+ * Time: 17:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CGrantMoneyController {
+
+}
---
+++ _core/_models/grant/CGrantMoney.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: aleksandr
+ * Date: 12.05.13
+ * Time: 17:43
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CGrantMoney extends CActiveModel{
+    protected $_table = TABLE_GRANT_MONEY;
+}
