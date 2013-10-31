fcaeddbe9faa866f241026c820ff18b10bb67999
Александр Бармин <barmin.alexander@gmail.com>
2013-10-31 12:51:54 +0600

    Сменил положение всех кешей в системе на папку tmp

--- _modules/_thumbnails/timthumb-config.php
+++ _modules/_thumbnails/timthumb-config.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../../core.php");
-    define("FILE_CACHE_DIRECTORY", "../../_core/_cache");
+    define("FILE_CACHE_DIRECTORY", TIMTHUMB_CACHE);
     define("DEBUG_ON", true);
     define("DEBUG_LEVEL", 3);
     define("LOCAL_FILE_BASE_DIRECTORY", CORE_CWD);
--- core.php
+++ core.php
@@ -9,7 +9,7 @@
      */
     define('CORE_DS', DIRECTORY_SEPARATOR);
     define('CORE_CWD', str_replace(CORE_DS."core.php", "", __FILE__));
-    define("CACHE_DIR", CORE_CWD.CORE_DS."_core".CORE_DS."_cache");
+    define("CACHE_DIR", CORE_CWD.CORE_DS."tmp".CORE_DS."cache");
     define('SMARTY_FOLDER', CORE_CWD.'/_core/_external/smarty');
     define('SMARTY_DIR', SMARTY_FOLDER.'/libs/');
     define('VIEWS_DIR', CORE_CWD.CORE_DS.'_core'.CORE_DS.'_views'.CORE_DS);
@@ -18,13 +18,14 @@
     define("JSON_CONTROLLERS_DIR", CORE_CWD.CORE_DS.'_core'.CORE_DS.'_json_controllers'.CORE_DS);
     define('AJAX_VIEW', "_ajax.html.php");
     define('SMARTY_TEMPLATES', VIEWS_DIR);
-    define('SMARTY_COMPILE', CACHE_DIR);
-    define('SMARTY_CACHE', SMARTY_FOLDER.'/cache/');
+    define('SMARTY_COMPILE', CORE_CWD.CORE_DS.'tmp'.CORE_DS.'smarty'.CORE_DS);
+    define('SMARTY_CACHE', CORE_CWD.CORE_DS.'tmp'.CORE_DS.'smarty'.CORE_DS);
     define("PHPMAILER_DIR", CORE_CWD.'/_core/_external/phpmailer');
     define("PRINT_ENGINE_WORD", CORE_CWD.CORE_DS.'_core'.CORE_DS.'_external'.CORE_DS.'phpword'.CORE_DS);
     define("PRINT_TEMPLATES_DIR", CORE_CWD.CORE_DS.'library'.CORE_DS.'templates'.CORE_DS);
     define("PRINT_DOCUMENTS_DIR", CORE_CWD.CORE_DS.'tmp'.CORE_DS.'print'.CORE_DS);
     define("ZIP_DOCUMENTS_DIR", CORE_CWD.CORE_DS.'tmp'.CORE_DS.'zip'.CORE_DS);
+    define("TIMTHUMB_CACHE", CORE_CWD.CORE_DS.'tmp'.CORE_DS.'timthumb'.CORE_DS);
     /**
      * Перекидываем настройки соединения с БД в константы
      */
@@ -94,7 +95,7 @@
             "timeout" => 360
         ),
         "smarty" => array(
-            "cacheEnabled" => false
+            "cacheEnabled" => true
         )
     );
     /**
