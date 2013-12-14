f5025b8502af944df65e21f3cd52bd44272f3432
Александр Бармин <barmin.alexander@gmail.com>
2013-12-14 16:57:56 +0600

    Новый интерфейс для валидаторов модели

---
+++ _core/_models/core/IModelValidator.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: aleksandr
+ * Date: 14.12.13
+ * Time: 16:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+interface IModelValidator extends IValidator{
+    public function onCreate(CModel $model);
+    public function onRead(CModel $model);
+    public function onUpdate(CModel $model);
+    public function onDelete(CModel $model);
+}
