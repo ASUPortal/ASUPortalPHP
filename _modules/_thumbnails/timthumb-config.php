6ed132db662fd0569ad658da79f74589f57b949a
Александр Бармин <barmin.alexander@gmail.com>
2014-01-26 22:42:03 +0600

    А вот так на боевом заработало

--- _modules/_thumbnails/timthumb-config.php
+++ _modules/_thumbnails/timthumb-config.php
@@ -3,6 +3,6 @@
     define("FILE_CACHE_DIRECTORY", TIMTHUMB_CACHE);
     define("DEBUG_ON", true);
     define("DEBUG_LEVEL", 3);
-    define("LOCAL_FILE_BASE_DIRECTORY", CORE_CWD);
+    define("LOCAL_FILE_BASE_DIRECTORY", str_replace(str_replace("/", "", ROOT_FOLDER), "", CORE_CWD));
     define("MAX_WIDTH", 5000);
     define("MAX_HEIGHT", 5000);
