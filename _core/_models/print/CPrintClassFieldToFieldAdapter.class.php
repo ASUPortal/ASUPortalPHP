5a12b75639dc0007950b013bf5ba807bb1c99cc8
Alexander Barmin <barmin.alexander@gmail.com>
2015-02-06 17:28:27 +0500

    Думал, что закоммитил, не закоммитил нужный класс адаптера

--- _core/_models/print/CPrintClassFieldToFieldAdapter.class.php
+++ _core/_models/print/CPrintClassFieldToFieldAdapter.class.php
@@ -6,6 +6,45 @@
  * Time: 17:40
  */
 
-class CPrintClassFieldToFieldAdapter {
+class CPrintClassFieldToFieldAdapter extends CPrintField{
+    private $_classField = null;
+    private $_context = null;
+    public $children = null;
+
+    public function __construct(IPrintClassField $classField, $context) {
+        $this->_classField = $classField;
+        $this->_context = $context;
+        /**
+         * В CPrintField эти члены публичные, их значения надо заполнить уже в конструкторе
+         */
+        $this->children = new CArrayList();
+        if ($this->getClassField()->getFieldType() == IPrintClassField::FIELD_TABLE) {
+            $this->type_id = "2";
+        } else {
+            $this->type_id = "1";
+        }
+    }
+
+    /**
+     * @return IPrintClassField
+     */
+    private function getClassField() {
+        return $this->_classField;
+    }
+
+    private function getContext() {
+        return $this->_context;
+    }
+
+    /**
+     * Делегируем вычисление классу-поля
+     *
+     * @param $object
+     * @return mixed
+     */
+    public function evaluateValue($object) {
+        return $this->getClassField()->execute($this->getContext());
+    }
+
 
 }
