db61979a792761d426e6861f2808e2aba632a3e1
Alexander Barmin <barmin.alexander@gmail.com>
2015-04-03 14:27:27 +0500

    Таблицу не ту указал у видов нагрузки

--- _core/_models/workplan/CWorkPlanTerm.class.php
+++ _core/_models/workplan/CWorkPlanTerm.class.php
@@ -4,7 +4,23 @@
  * User: abarmin
  * Date: 03.04.15
  * Time: 15:05
+ *
+ * @property CArrayList types
  */
 class CWorkPlanTerm extends CActiveModel{
     protected $_table = TABLE_WORK_PLAN_TERMS;
+
+    protected function relations(){
+        return array(
+            "types" => array(
+                "relationPower" => RELATION_HAS_MANY,
+                "storageProperty" => "_types",
+                "storageTable" => TABLE_WORK_PLAN_TERM_LOADS,
+                "storageCondition" => "term_id = " . (is_null($this->getId()) ? 0 : $this->getId()),
+                "targetClass" => "CWorkPlanTermLoad"
+            ),
+        );
+    }
+
+
 }
