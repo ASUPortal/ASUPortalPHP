c41f01e2368ab1a297619819e20ff880ec4fb2c1
Alexander Barmin <barmin.alexander@gmail.com>
2015-04-03 14:25:07 +0500

    Виды нагрузки в семестре

---
+++ _core/_models/workplan/CWorkPlanTermLoad.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abarmin
+ * Date: 03.04.15
+ * Time: 15:24
+ */
+
+class CWorkPlanTermLoad extends CActiveModel{
+    protected $_table = TABLE_WORK_PLAN_TERM_LOADS;
+}
