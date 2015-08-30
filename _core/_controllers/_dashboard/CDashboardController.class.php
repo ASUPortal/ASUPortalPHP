a086bbe7e38d688b0c342929aa4189a42d59208f
shamidanow <shamidanow@gmail.com>
2015-08-30 20:54:19 +0500

    Поправка к рабочему столу

--- _core/_controllers/_dashboard/CDashboardController.class.php
+++ _core/_controllers/_dashboard/CDashboardController.class.php
@@ -97,6 +97,36 @@ class CDashboardController extends CBaseController {
         $this->addJSInclude("_modules/_dashboard/script.js");
 		$this->renderView("_dashboard/index.tpl");
 	}
+	public function actionTasks() {
+		$set = new CRecordSet();
+		$queryForGroup = new CQuery();
+		$queryForGroup->select("distinct(tasks.id) as id, tasks.name as name, tasks.url as url")
+			->from(TABLE_USER_GROUP_HAS_ROLES." as groupTasks")
+			->innerJoin(TABLE_USER_ROLES." as tasks", "groupTasks.task_id=tasks.id")
+			->innerJoin(TABLE_USER_IN_GROUPS." as userGroup", "userGroup.user_id=".CSession::getCurrentUser()->id." and groupTasks.user_group_id=userGroup.group_id and groupTasks.task_rights_id!=0")
+			->condition('tasks.hidden!=1')
+			->order("tasks.name asc");
+		$set->setQuery($queryForGroup);
+		$setForUser = new CRecordSet();
+		$queryForUser = new CQuery();
+		$queryForUser->select("distinct(tasks.id) as id, tasks.name as name, tasks.url as url")
+			->from(TABLE_USER_HAS_ROLES." as userTasks")
+			->innerJoin(TABLE_USER_ROLES." as tasks", "userTasks.task_id=tasks.id")
+			->condition('tasks.hidden!=1 and userTasks.user_id="'.CSession::getCurrentUser()->id.'" and userTasks.task_rights_id!=0')
+			->order("tasks.name asc");
+		$setForUser->setQuery($queryForUser);
+		$tasks = new CArrayList();
+		foreach ($set->getItems() as $item) {
+			$task = new CUserRole($item);
+			$tasks->add($task->getId(), $task);
+		}
+		foreach ($setForUser->getItems() as $item) {
+			$task = new CUserRole($item);
+			$tasks->add($task->getId(), $task);
+		}
+		$this->setData("tasks", $tasks);
+		$this->renderView("_dashboard/tasks.tpl");
+	}
 	public function actionList() {
 		$set = CActiveRecordProvider::getWithCondition(TABLE_DASHBOARD, "user_id = ".CSession::getCurrentUser()->getId()." and parent_id = 0");
         $items = new CArrayList();
