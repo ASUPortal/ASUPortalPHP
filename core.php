324d8e6d7abb85ca679724ee825fbbf8f9d7d349
Alexander Barmin <barmin.alexander@gmail.com>
2015-09-13 14:28:35 +0500

    На всякий случай добавил кастомный обработчико ошибок

--- core.php
+++ core.php
@@ -423,3 +423,41 @@
      */
     define("ROLE_PAGES_ADMIN", "pages_admin");
     define("ROLE_GRANTS_ADMIN", "grants_admin");
+    /**
+     * Кастомный обработчик ошибок
+     */
+    if (APP_DEBUG) {
+        set_error_handler("customErrorHandler");
+        set_exception_handler("customExceptionHandler");
+    }
+    function customErrorHandler($errno, $errstr, $errfile, $errline) {
+        if (!(error_reporting() & $errno)) {
+            // Этот код ошибки не включен в error_reporting
+            return;
+        }
+
+        switch ($errno) {
+            case E_USER_ERROR:
+                echo "<b>My ERROR</b> [$errno] $errstr<br />\n";
+                echo "  Фатальная ошибка в строке $errline файла $errfile";
+                echo ", PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />\n";
+                echo "Завершение работы...<br />\n";
+                exit(1);
+                break;
+
+            case E_USER_WARNING:
+                echo "<b>My WARNING</b> [$errno] $errstr<br />\n";
+                break;
+
+            case E_USER_NOTICE:
+                echo "<b>My NOTICE</b> [$errno] $errstr<br />\n";
+                break;
+
+            default:
+                echo "Неизвестная ошибка: [$errno] $errstr<br />\n";
+                break;
+        }
+    }
+    function customExceptionHandler($exception) {
+        echo "Неперехватываемое исключение: " , $exception->getMessage(), "\n";
+    }
