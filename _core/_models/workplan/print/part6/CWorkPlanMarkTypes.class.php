adc7637c28006fd5cd97d46dda3289e9e8f8d43a
shamidanow <shamidanow@gmail.com>
2015-10-01 00:48:55 +0500

    0001143 Описатель CWorkPlanMarkTypes

--- _core/_models/workplan/print/part6/CWorkPlanMarkTypes.class.php
+++ _core/_models/workplan/print/part6/CWorkPlanMarkTypes.class.php
@@ -24,26 +24,16 @@ class CWorkPlanMarkTypes extends CAbstractPrintClassField {
     public function execute($contextObject)
     {
 		$result = array();
-        $set = new CRecordSet();
-        $query = new CQuery();
-        $set->setQuery($query);
-        $query->select("t.*")
-	        ->from(TABLE_WORK_PLAN_MARK_TYPES." as t")
-	        ->order("t.id asc")
-	        ->condition("plan_id=".$contextObject->getId());
-        $objects = new CArrayList();
-        foreach ($set->getItems() as $ar) {
-        	$object = new CWorkPlanMarkType($ar);
-        	$objects->add($object->getId(), $object);
-        }
-        foreach ($objects->getItems() as $row) {
-        	$dataRow = array();
-        	$dataRow[0] = count($result) + 1;
-        	$dataRow[1] = $row->type;
-        	$dataRow[2] = $row->form;
-        	$dataRow[3] = implode(", ", $row->funds->getItems());
-        	$dataRow[4] = implode(", ", $row->places->getItems());
-        	$result[] = $dataRow;
+        if (!is_null($contextObject->markTypes)) {
+        	foreach ($contextObject->markTypes->getItems() as $row) {
+				$dataRow = array();
+				$dataRow[0] = count($result) + 1;
+				$dataRow[1] = $row->type;
+				$dataRow[2] = $row->form;
+				$dataRow[3] = implode(", ", $row->funds->getItems());
+				$dataRow[4] = implode(", ", $row->places->getItems());
+				$result[] = $dataRow;
+        	}
         }
         return $result;
     }
