667d80fcc2efb473d0658d0d0b1e2a3cca8eaf52
shamidanow <shamidanow@gmail.com>
2015-10-03 01:38:49 +0500

    0001141 Описатель CWorkPlanTotalCredits и подобные

--- _core/_models/workplan/print/part4/total/CWorkPlanTermSectionsLabWork.class.php
+++ _core/_models/workplan/print/part4/total/CWorkPlanTermSectionsLabWork.class.php
@@ -25,26 +25,18 @@ class CWorkPlanTermSectionsLabWork extends CAbstractPrintClassField {
     {
 		$termSectionsData = new CArrayList();
         foreach ($contextObject->terms->getItems() as $term) {
-            $query = new CQuery();
-            $select = array();
-            $select[] = "section.sectionIndex";
-            $select[] = "section.name";
-            $select[] = "sum(if(term.alias in ('lecture', 'practice', 'labwork'), l.value, 0)) + sum(selfedu.question_hours) as total";
-            $select[] = "sum(if(term.alias = 'lecture', l.value, 0)) as lecture";
-            $select[] = "sum(if(term.alias = 'practice', l.value, 0)) as practice";
-            $select[] = "sum(if(term.alias = 'labwork', l.value, 0)) as labwork";
-            $select[] = "sum(selfedu.question_hours) as selfedu";
-            $query->select(join(", ", $select))
-                ->from(TABLE_WORK_PLAN_CONTENT_SECTIONS." as section")
-                ->innerJoin(TABLE_WORK_PLAN_CONTENT_LOADS." as l", "l.section_id = section.id")
-                ->innerJoin(TABLE_TAXONOMY_TERMS." as term", "term.id = l.load_type_id")
-                ->leftJoin(TABLE_WORK_PLAN_SELFEDUCATION." as selfedu", "selfedu.load_id = l.id")
-                ->group("l.section_id")
-                ->condition("l.term_id = ".$term->getId());
-            $items = $query->execute();
-            if ($items->getCount() > 0) {
-                $termSectionsData->add($term->getId(), $items);
-            }
+        	$query = new CQuery();
+        	$query->select("sum(if(term.alias = 'labwork', l.value, 0)) as labwork")
+	        	->from(TABLE_WORK_PLAN_CONTENT_SECTIONS." as section")
+	        	->innerJoin(TABLE_WORK_PLAN_CONTENT_LOADS." as l", "l.section_id = section.id")
+	        	->innerJoin(TABLE_TAXONOMY_TERMS." as term", "term.id = l.load_type_id")
+	        	->leftJoin(TABLE_WORK_PLAN_SELFEDUCATION." as selfedu", "selfedu.load_id = l.id")
+	        	->group("l.section_id")
+	        	->condition("l.term_id = ".$term->getId());
+        	$items = $query->execute();
+        	if ($items->getCount() > 0) {
+        		$termSectionsData->add($term->getId(), $items);
+        	}
         }
         $result = 0;
         foreach ($termSectionsData->getItems() as $termId=>$termData) {
