a7a6f1d26fb43b84cf3ed4cf89346902d283eb0c
shamidanow <shamidanow@gmail.com>
2016-06-10 13:33:47 +0500

    Вывод по отдельности ЗУВов в описателе компетенций

--- _core/_models/workplan/print/part3/CWorkPlanCompetentionsProceduresEvaluation.class.php
+++ _core/_models/workplan/print/part3/CWorkPlanCompetentionsProceduresEvaluation.class.php
@@ -28,26 +28,48 @@ class CWorkPlanCompetentionsProceduresEvaluation extends CAbstractPrintClassFiel
         	foreach ($contextObject->competentions->getItems() as $item) {
         		//формируемые компетенции рабочей программы
         		if ($item->type == 0) {
-        			$dataRow = array();
-	        		if (!is_null($item->competention)) {
-	        			$str = $item->competention->getValue();
-	        			//берем код компетенции - текст из скобок
-	        			preg_match('/\((.+)\)/', $str, $m);
-	        			if (!is_null($item->level)) {
-	        				$dataRow[0] = $m[1].", ".$item->level->getValue();
-	        			} else {
-	        				$dataRow[0] = $m[1];
-	        			}
-	        		}
-	        		if (!is_null($item->knowledges)) {
-	        			$dataRow[1] = "Знать: ".implode("; ", $item->knowledges->getItems())."; ";
-	        			$dataRow[1] .= "Уметь: ".implode("; ", $item->skills->getItems())."; ";
-	        			$dataRow[1] .= "Владеть: ".implode("; ", $item->experiences->getItems())."; ";
-	        		}
-	        		$dataRow[2] = $item->type_task;
-	        		$dataRow[3] = $item->procedure_eval;
-	        		$dataRow[4] = $item->criteria_eval;
-        			$result[] = $dataRow;
+        			if ($item->type == 0) {
+        				$dataRow1 = array();
+        				$competention = "";
+        				if (!is_null($item->competention)) {
+        					$str = $item->competention->getValue();
+        					//берем код компетенции - текст из скобок
+        					preg_match('/\((.+)\)/', $str, $m);
+        					if (!is_null($item->level)) {
+        						$dataRow1[0] = $m[1].", ".$item->level->getValue();
+        					} else {
+        						$dataRow1[0] = $m[1];
+        					}
+        					$competention = $dataRow1[0];
+        				}
+        				if (!is_null($item->knowledges)) {
+        					$dataRow1[1] = "Знать: ".implode("; ", $item->knowledges->getItems());
+        				}
+        				$dataRow1[2] = $item->type_task;
+        				$dataRow1[3] = $item->procedure_eval;
+        				$dataRow1[4] = $item->criteria_eval;
+        				$result[] = $dataRow1;
+        			
+        				$dataRow2 = array();
+        				$dataRow2[0] = $competention;
+        				if (!is_null($item->knowledges)) {
+        					$dataRow2[1] = "Уметь: ".implode("; ", $item->skills->getItems());
+        				}
+        				$dataRow2[2] = $item->type_task;
+        				$dataRow2[3] = $item->procedure_eval;
+        				$dataRow2[4] = $item->criteria_eval;
+        				$result[] = $dataRow2;
+        			
+        				$dataRow3 = array();
+        				$dataRow3[0] = $competention;
+        				if (!is_null($item->knowledges)) {
+        					$dataRow3[1] = "Владеть: ".implode("; ", $item->experiences->getItems());
+        				}
+        				$dataRow3[2] = $item->type_task;
+        				$dataRow3[3] = $item->procedure_eval;
+        				$dataRow3[4] = $item->criteria_eval;
+        				$result[] = $dataRow3;
+        			}
         		}
         	}
         }
