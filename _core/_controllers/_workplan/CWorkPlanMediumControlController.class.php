d60de35b05c67b2ec5967b2c20b9eb7d078dec40
shamidanow <shamidanow@gmail.com>
2016-10-31 17:19:24 +0500

    Контроллер промежуточного контроля

---
+++ _core/_controllers/_workplan/CWorkPlanMediumControlController.class.php
@@ -0,0 +1,116 @@
+<?php
+class CWorkPlanMediumControlController extends CBaseController{
+	protected $_isComponent = true;
+	
+    public function __construct() {
+        if (!CSession::isAuth()) {
+            $action = CRequest::getString("action");
+            if ($action == "") {
+                $action = "index";
+            }
+            if (!in_array($action, $this->allowedAnonymous)) {
+                $this->redirectNoAccess();
+            }
+        }
+
+        $this->_smartyEnabled = true;
+        $this->setPageTitle("Промежуточный контроль");
+
+        parent::__construct();
+    }
+    public function actionIndex() {
+        $set = new CRecordSet();
+        $query = new CQuery();
+        $set->setQuery($query);
+        $query->select("t.*")
+            ->from(TABLE_WORK_PLAN_MEDIUM_CONTROL." as t")
+            ->condition("plan_id=".CRequest::getInt("plan_id")." and _deleted=0")
+            ->order("t.ordering asc");
+        $objects = new CArrayList();
+        foreach ($set->getPaginated()->getItems() as $ar) {
+            $object = new CWorkPlanMediumControl($ar);
+            $objects->add($object->getId(), $object);
+        }
+        $this->setData("objects", $objects);
+        $this->setData("paginator", $set->getPaginator());
+        /**
+         * Генерация меню
+         */
+        $this->addActionsMenuItem(array(
+        	"title" => "Обновить",
+        	"link" => "workplanmediumcontrol.php?action=index&plan_id=".CRequest::getInt("plan_id"),
+        	"icon" => "actions/view-refresh.png"
+        ));
+        $this->addActionsMenuItem(array(
+            "title" => "Добавить",
+            "link" => "workplanmediumcontrol.php?action=add&id=".CRequest::getInt("plan_id"),
+            "icon" => "actions/list-add.png"
+        ));
+        /**
+         * Отображение представления
+         */
+        $this->renderView("_corriculum/_workplan/mediumControl/index.tpl");
+    }
+    public function actionAdd() {
+        $object = new CWorkPlanMediumControl();
+        $object->plan_id = CRequest::getInt("id");
+        $plan = CWorkPlanManager::getWorkplan(CRequest::getInt("id"));
+        $object->ordering = $plan->mediumControls->getCount() + 1;
+        $this->setData("object", $object);
+        /** 
+         * Генерация меню
+         */
+        $this->addActionsMenuItem(array(
+            "title" => "Назад",
+            "link" => "workplanmediumcontrol.php?action=index&plan_id=".$object->plan_id,
+            "icon" => "actions/edit-undo.png"
+        ));
+        /**
+         * Отображение представления
+         */
+        $this->renderView("_corriculum/_workplan/mediumControl/add.tpl");
+    }
+    public function actionEdit() {
+        $object = CBaseManager::getWorkPlanMediumControl(CRequest::getInt("id"));
+        $this->setData("object", $object);
+        /**
+         * Генерация меню
+         */
+        $this->addActionsMenuItem(array(
+            "title" => "Назад",
+            "link" => "workplanmediumcontrol.php?action=index&plan_id=".$object->plan_id,
+            "icon" => "actions/edit-undo.png"
+        ));
+        /**
+         * Отображение представления
+         */
+        $this->renderView("_corriculum/_workplan/mediumControl/edit.tpl");
+    }
+    public function actionDelete() {
+        $object = CBaseManager::getWorkPlanMediumControl(CRequest::getInt("id"));
+        $plan = $object->plan;
+        $object->markDeleted(true);
+        $object->save();
+        $order = 1;
+        foreach ($plan->mediumControls as $mediumControl) {
+        	$mediumControl->ordering = $order++;
+        	$mediumControl->save();
+        }
+        $this->redirect("workplanmediumcontrol.php?action=index&plan_id=".$plan->getId());
+    }
+    public function actionSave() {
+        $object = new CWorkPlanMediumControl();
+        $object->setAttributes(CRequest::getArray($object::getClassName()));
+        if ($object->validate()) {
+            $object->save();
+            if ($this->continueEdit()) {
+                $this->redirect("workplanmediumcontrol.php?action=edit&id=".$object->getId());
+            } else {
+                $this->redirect("workplanmediumcontrol.php?action=index&plan_id=".$object->plan_id);
+            }
+            return true;
+        }
+        $this->setData("object", $object);
+        $this->renderView("_corriculum/_workplan/mediumControl/edit.tpl");
+    }
+}
