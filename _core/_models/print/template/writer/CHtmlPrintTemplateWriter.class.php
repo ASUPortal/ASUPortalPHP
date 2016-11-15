6ad4a4a0e62eed6366a6c7e02bc9f88e4f2ad912
shamidanow <shamidanow@gmail.com>
2016-11-16 03:07:46 +0500

    Поправка

--- _core/_models/print/template/writer/CHtmlPrintTemplateWriter.class.php
+++ _core/_models/print/template/writer/CHtmlPrintTemplateWriter.class.php
@@ -49,12 +49,6 @@ class CHtmlPrintTemplateWriter implements IPrintTemplateWriter {
      * @return String
      */
     public function save(IPrintTemplate $template, $filename) {
-		/*require CORE_CWD."/_core/_external/convertio/autoload.php";
-		use \Convertio\Convertio;
-    	
-		// You can obtain API Key here: https://convertio.co/api/
-		$API = new Convertio("_YOUR_API_KEY_");           
-		$API->start($template->_tempFileName, 'odt')->wait()->download(PRINT_DOCUMENTS_DIR.$filename)->delete();*/
 		rename($template->_tempFileName, PRINT_DOCUMENTS_DIR.$filename);
     }
     
