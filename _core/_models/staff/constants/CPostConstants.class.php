290ce3e738489d955417e896d3314dc18e7c3458
shamidanow <shamidanow@gmail.com>
2017-01-10 12:38:00 +0500

    Константы должностей сотрудников

---
+++ _core/_models/staff/constants/CPostConstants.class.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Константы должностей сотрудников
+ */
+interface CPostConstants {
+	const HEAD_OF_DEPARTMENT = "head_of_department";
+	const ACTING_HEAD_OF_DEPARTMENT = "acting_head_of_department";
+}
--- _core/_services/CStaffService.class.php
+++ _core/_services/CStaffService.class.php
@@ -15,8 +15,8 @@ class CStaffService {
     public static function getHeadOrActingOfDepartment($withPost) {
         $person = "";
         
-        $headOfDepartment = CTaxonomyManager::getLegacyTaxonomy(TABLE_POSTS)->getTerm("head_of_department");
-        $actingHeadOfDepartment = CTaxonomyManager::getLegacyTaxonomy(TABLE_POSTS)->getTerm("acting_head_of_department");
+        $headOfDepartment = CTaxonomyManager::getLegacyTaxonomy(TABLE_POSTS)->getTerm(CPostConstants::HEAD_OF_DEPARTMENT);
+        $actingHeadOfDepartment = CTaxonomyManager::getLegacyTaxonomy(TABLE_POSTS)->getTerm(CPostConstants::ACTING_HEAD_OF_DEPARTMENT);
         
         if (!is_null(CStaffManager::getPersonByPostId($actingHeadOfDepartment->getId()))) {
         	if ($withPost) {
@@ -41,7 +41,7 @@ class CStaffService {
      * @return string
      */
     public static function getHeadOfDepartment() {
-        $headOfDepartment = CTaxonomyManager::getLegacyTaxonomy(TABLE_POSTS)->getTerm("head_of_department");
+        $headOfDepartment = CTaxonomyManager::getLegacyTaxonomy(TABLE_POSTS)->getTerm(CPostConstants::HEAD_OF_DEPARTMENT);
         $person = CStaffManager::getPersonByPostId($headOfDepartment->getId())->getNameShort();
         return $person;
     }
