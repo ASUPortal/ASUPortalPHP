57dcc3a597de1338727dfecf3f8ea5587802f6c1
shamidanow <shamidanow@gmail.com>
2017-01-17 19:04:20 +0500

    Комментарий

--- _core/_external/vk/Vkapi.php
+++ _core/_external/vk/Vkapi.php
@@ -1,7 +1,17 @@
 <?php
-
+/**
+ * Класс для работы с API ВКонтакте, подробнее здесь: https://vk.com/dev/first_guide
+ *
+ */
 class Vkapi {
 
+	/**
+	 * Вызов метода API ВКонтакте, список методов можно посмотреть здесь: https://vk.com/dev/methods
+	 * Для идентификации в API используется параметр access_token зарегистрированного Standalone-приложения
+	 * 
+	 * @param unknown $name
+	 * @param array $params
+	 */
 	public static function invoke ($name, array $params = array()) {
 		$params['access_token'] = CSettingsManager::getSettingValue("access_token_vk");
 
