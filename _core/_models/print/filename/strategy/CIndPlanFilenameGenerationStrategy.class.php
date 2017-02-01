6239ef46555e3e505854f62098a29068287731d4
shamidanow <shamidanow@gmail.com>
2017-02-01 23:52:23 +0500

    Поправка

--- _core/_models/print/filename/strategy/CIndPlanFilenameGenerationStrategy.class.php
+++ _core/_models/print/filename/strategy/CIndPlanFilenameGenerationStrategy.class.php
@@ -25,6 +25,7 @@ class CIndPlanFilenameGenerationStrategy implements IPrintFilenameGenerationStra
 		$person = $object->getNameShort();
 		//$person = CUtils::toTranslit($object->getNameShort());
 		$load = CIndPlanManager::getLoad(CRequest::getInt("planId"));
+		$year = "";
 		if (!is_null(CTaxonomyManager::getYear($load->year_id))) {
 			$year = CTaxonomyManager::getYear($load->year_id)->getValue();
 		}
