e1f906491cdd005acd41441656de4579fa028339
shamidanow <shamidanow@gmail.com>
2017-07-24 23:36:15 +0500

    Установка признака последней версии для записей нагрузки

---
+++ _install/fixtures/fixture_20170724_000051.sql
@@ -0,0 +1,5 @@
+UPDATE `asu`.`pl_corriculum_workload` SET `_is_last_version` = 1;
+
+UPDATE `asu`.`pl_corriculum_workload_by_type` SET `_is_last_version` = 1;
+
+UPDATE `asu`.`pl_corriculum_workload_groups` SET `_is_last_version` = 1;
