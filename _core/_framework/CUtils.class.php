0a6308e86af71bea7f1b1ba0f772b3dc161c5575
shamidanow <shamidanow@gmail.com>
2017-09-19 15:19:44 +0500

    Преобразование изображений формата bmp для thumnails

--- _core/_framework/CUtils.class.php
+++ _core/_framework/CUtils.class.php
@@ -795,83 +795,76 @@ class CUtils {
      */
     public static function getMimetype($filename) {
         $filetype = "";
-        if (class_exists("finfo")) {
-            $finfo = new finfo(FILEINFO_MIME);
-            $filetype = $finfo->file($filename);
-            $filetype = CUtils::strLeft($filetype, ";");
-            $filetype = str_replace("/", "-", $filetype);
-        } elseif (function_exists("mime_content_type")) {
-            $filetype = mime_content_type($filename);
-            $filetype = str_replace("/", "-", $filetype);
+        $mime_types = array(
+        
+        		'txt' => 'text/plain',
+        		'htm' => 'text/html',
+        		'html' => 'text/html',
+        		'php' => 'text/html',
+        		'css' => 'text/css',
+        		'js' => 'text/x-javascript',
+        		'json' => 'application/json',
+        		'xml' => 'text/xml',
+        		'swf' => 'application/x-flash-video',
+        		'flv' => 'video/x-flv',
+        
+        		// images
+        		'png' => 'image/png',
+        		'jpe' => 'image/jpeg',
+        		'jpeg' => 'image/jpeg',
+        		'jpg' => 'image/jpeg',
+        		'gif' => 'image/gif',
+        		'bmp' => 'image/bmp',
+        		'ico' => 'image/x-ico',
+        		'tiff' => 'image/tiff',
+        		'tif' => 'image/tiff',
+        		'svg' => 'image/svg+xml',
+        		'svgz' => 'image/svg+xml',
+        
+        		// archives
+        		'zip' => 'application/x-zip',
+        		'7z' => 'application/x-7zip',
+        		'tar' => 'application/x-tar',
+        		'rar' => 'application/x-rar',
+        		'exe' => 'application/x-executable',
+        		'msi' => 'application/x-msdownload',
+        		'cab' => 'application/vnd.ms-cab-compressed',
+        
+        		// audio/video
+        		'mp3' => 'audio/x-mpeg',
+        		'qt' => 'video/quicktime',
+        		'mov' => 'video/quicktime',
+        
+        		// adobe
+        		'pdf' => 'application/pdf',
+        		'psd' => 'image/vnd.adobe.photoshop',
+        		'ai' => 'application/postscript',
+        		'eps' => 'application/postscript',
+        		'ps' => 'application/postscript',
+        
+        		// ms office
+        		'doc' => 'application/msword',
+        		'rtf' => 'text/richtext',
+        		'xls' => 'application/vnd.ms-excel',
+        		'ppt' => 'application/vnd.ms-powerpoint',
+        
+        		// ms office new
+        		'docx' => 'application/msword',
+        		'xlsx' => 'application/vnd.ms-excel',
+        		'pptx' => 'application/vnd.ms-powerpoint',
+        
+        		// open office
+        		'odt' => 'x-office-document',
+        		'ods' => 'x-office-spreadsheet',
+        		'odp' => 'x-office-presentation',
+        		'odg' => 'x-office-drawing',
+        );
+        $ext = strtolower(array_pop(explode('.', $filename)));
+        if (array_key_exists($ext, $mime_types)) {
+        	$filetype = $mime_types[$ext];
+        	$filetype = str_replace("/", "-", $filetype);
         } else {
-            $mime_types = array(
-
-                'txt' => 'text/plain',
-                'htm' => 'text/html',
-                'html' => 'text/html',
-                'php' => 'text/html',
-                'css' => 'text/css',
-                'js' => 'application/javascript',
-                'json' => 'application/json',
-                'xml' => 'application/xml',
-                'swf' => 'application/x-shockwave-flash',
-                'flv' => 'video/x-flv',
-
-                // images
-                'png' => 'image/png',
-                'jpe' => 'image/jpeg',
-                'jpeg' => 'image/jpeg',
-                'jpg' => 'image/jpeg',
-                'gif' => 'image/gif',
-                'bmp' => 'image/bmp',
-                'ico' => 'image/vnd.microsoft.icon',
-                'tiff' => 'image/tiff',
-                'tif' => 'image/tiff',
-                'svg' => 'image/svg+xml',
-                'svgz' => 'image/svg+xml',
-
-                // archives
-                'zip' => 'application/zip',
-                'rar' => 'application/x-rar-compressed',
-                'exe' => 'application/x-msdownload',
-                'msi' => 'application/x-msdownload',
-                'cab' => 'application/vnd.ms-cab-compressed',
-
-                // audio/video
-                'mp3' => 'audio/mpeg',
-                'qt' => 'video/quicktime',
-                'mov' => 'video/quicktime',
-
-                // adobe
-                'pdf' => 'application/pdf',
-                'psd' => 'image/vnd.adobe.photoshop',
-                'ai' => 'application/postscript',
-                'eps' => 'application/postscript',
-                'ps' => 'application/postscript',
-
-                // ms office
-                'doc' => 'application/msword',
-                'rtf' => 'application/rtf',
-                'xls' => 'application/vnd.ms-excel',
-                'ppt' => 'application/vnd.ms-powerpoint',
-
-                // ms office new
-                'docx' => 'application/msword',
-                'xlsx' => 'application/vnd.ms-excel',
-                'pptx' => 'application/vnd.ms-powerpoint',
-
-                // open office
-                'odt' => 'application/vnd.oasis.opendocument.text',
-                'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
-            );
-
-            $ext = strtolower(array_pop(explode('.', $filename)));
-            if (array_key_exists($ext, $mime_types)) {
-                $filetype = $mime_types[$ext];
-                $filetype = str_replace("/", "-", $filetype);
-            } else {
-                $filetype = "unknown";
-            }
+        	$filetype = "unknown";
         }
         return $filetype;
     }
@@ -968,6 +961,131 @@ class CUtils {
      */
     public static function getDocumentRoot() {
     	return $_SERVER["DOCUMENT_ROOT"];
+    }
+    
+    /**
+     * Создание изображения из формата BMP
+     * 
+     * @param string $filename
+     * @return resource|boolean
+     */
+    public static function imageCreateFromBMP($filename) {
+    	$tmp_name = tempnam("/tmp", "GD");
+    	if(CUtils::convertBMP2GD($filename, $tmp_name)) {
+    		$img = imagecreatefromgd($tmp_name);
+    		unlink($tmp_name);
+    		return $img;
+    	} return false;
+    }
+    
+    /**
+     * Преобразование изображения формата BMP
+     * 
+     * @param string $src
+     * @param string $dest
+     * @return boolean
+     */
+    public static function convertBMP2GD($src, $dest = false) {
+    	if(!($src_f = fopen($src, "rb"))) {
+    		return false;
+    	}
+    	if(!($dest_f = fopen($dest, "wb"))) {
+    		return false;
+    	}
+    	$header = unpack("vtype/Vsize/v2reserved/Voffset", fread($src_f, 14));
+    	$info = unpack("Vsize/Vwidth/Vheight/vplanes/vbits/Vcompression/Vimagesize/Vxres/Vyres/Vncolor/Vimportant", fread($src_f, 40));
+    
+    	extract($info);
+    	extract($header);
+    
+    	if($type != 0x4D42) { // signature "BM"
+    		return false;
+    	}
+    
+    	$palette_size = $offset - 54;
+    	$ncolor = $palette_size / 4;
+    	$gd_header = "";
+    	// true-color vs. palette
+    	$gd_header .= ($palette_size == 0) ? "\xFF\xFE" : "\xFF\xFF";
+    	$gd_header .= pack("n2", $width, $height);
+    	$gd_header .= ($palette_size == 0) ? "\x01" : "\x00";
+    	if($palette_size) {
+    		$gd_header .= pack("n", $ncolor);
+    	}
+    	// no transparency
+    	$gd_header .= "\xFF\xFF\xFF\xFF";
+    
+    	fwrite($dest_f, $gd_header);
+    
+    	if($palette_size) {
+    		$palette = fread($src_f, $palette_size);
+    		$gd_palette = "";
+    		$j = 0;
+    		while($j < $palette_size) {
+    			$b = $palette{$j++};
+    			$g = $palette{$j++};
+    			$r = $palette{$j++};
+    			$a = $palette{$j++};
+    			$gd_palette .= "$r$g$b$a";
+    		}
+    		$gd_palette .= str_repeat("\x00\x00\x00\x00", 256 - $ncolor);
+    		fwrite($dest_f, $gd_palette);
+    	}
+    
+    	$scan_line_size = (($bits * $width) + 7) >> 3;
+    	$scan_line_align = ($scan_line_size & 0x03) ? 4 - ($scan_line_size &
+    			0x03) : 0;
+    
+    	for($i = 0, $l = $height - 1; $i < $height; $i++, $l--) {
+    		// BMP stores scan lines starting from bottom
+    		fseek($src_f, $offset + (($scan_line_size + $scan_line_align) *
+    				$l));
+    		$scan_line = fread($src_f, $scan_line_size);
+    		if($bits == 24) {
+    			$gd_scan_line = "";
+    			$j = 0;
+    			while($j < $scan_line_size) {
+    				$b = $scan_line{$j++};
+    				$g = $scan_line{$j++};
+    				$r = $scan_line{$j++};
+    				$gd_scan_line .= "\x00$r$g$b";
+    			}
+    		}
+    		else if($bits == 8) {
+    			$gd_scan_line = $scan_line;
+    		}
+    		else if($bits == 4) {
+    			$gd_scan_line = "";
+    			$j = 0;
+    			while($j < $scan_line_size) {
+    				$byte = ord($scan_line{$j++});
+    				$p1 = chr($byte >> 4);
+    				$p2 = chr($byte & 0x0F);
+    				$gd_scan_line .= "$p1$p2";
+    			} $gd_scan_line = substr($gd_scan_line, 0, $width);
+    		}
+    		else if($bits == 1) {
+    			$gd_scan_line = "";
+    			$j = 0;
+    			while($j < $scan_line_size) {
+    				$byte = ord($scan_line{$j++});
+    				$p1 = chr((int) (($byte & 0x80) != 0));
+    				$p2 = chr((int) (($byte & 0x40) != 0));
+    				$p3 = chr((int) (($byte & 0x20) != 0));
+    				$p4 = chr((int) (($byte & 0x10) != 0));
+    				$p5 = chr((int) (($byte & 0x08) != 0));
+    				$p6 = chr((int) (($byte & 0x04) != 0));
+    				$p7 = chr((int) (($byte & 0x02) != 0));
+    				$p8 = chr((int) (($byte & 0x01) != 0));
+    				$gd_scan_line .= "$p1$p2$p3$p4$p5$p6$p7$p8";
+    			} $gd_scan_line = substr($gd_scan_line, 0, $width);
+    		}
+    
+    		fwrite($dest_f, $gd_scan_line);
+    	}
+    	fclose($src_f);
+    	fclose($dest_f);
+    	return true;
     }
     
 }
