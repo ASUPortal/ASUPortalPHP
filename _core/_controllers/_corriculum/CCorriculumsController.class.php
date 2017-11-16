410765faedb1f9b7f8f41e98e9afdfcc7cb3a9a1
shamidanow <shamidanow@gmail.com>
2017-11-16 18:45:45 +0500

    Поправка к удалению учебного плана

--- _core/_controllers/_corriculum/CCorriculumsController.class.php
+++ _core/_controllers/_corriculum/CCorriculumsController.class.php
@@ -493,16 +493,22 @@ class CCorriculumsController extends CBaseController {
         				$labor->remove();
         			}
         			/**
-        			 * Удаляем дочерние дисциплины из родительской
-        			 */
-        			foreach ($discipline->children->getItems() as $child) {
-        				$child->remove();
-        			}
-        			/**
         			 * Удаляем рабочие программы из дисциплин
         			 */
         			foreach ($discipline->plans->getItems() as $plan) {
         				$plan->remove();
+        			}
+        			/**
+        			 * Удаляем дочерние дисциплины из родительской
+        			 */
+        			foreach ($discipline->children->getItems() as $child) {
+        				/**
+        				 * Удаляем рабочие программы из дочерних дисциплин
+        				 */
+        				foreach ($child->plans->getItems() as $plan) {
+        					$plan->remove();
+        				}
+        				$child->remove();
         			}
         			$discipline->remove();
         		}
